8d3de0a6aa12cce09c6c517c479044f32f4ace75
vfsettle <56850679+vfsettle@users.noreply.github.com>
2020-03-26 14:52:39 -0700

    Create Wk3_inheritance2.py
    Week 3 - Inheritance Part 2

---
+++ Wk3_inheritance2.py
@@ -0,0 +1,41 @@
+class Spell:
+    def __init__(self, incantation, name):
+        self.name = name
+        self.incantation = incantation
+
+    def __str__(self):
+        return self.name + ' ' + self.incantation + '\n' + self.get_description()
+        #return self.get_description()
+
+    def get_description(self):
+        return 'No description'
+
+    def execute(self):
+        print (self.incantation)
+
+class Accio(Spell):
+    def __init__(self):
+        Spell.__init__(self, 'Accio', 'Summoning Charm')
+
+    def get_description(self):
+        return 'This charm summons an object to the caster, potentially over a significant distance.'
+
+    def __str__(self):
+        return '\n' + self.get_description()
+
+class Confundo(Spell):
+    def __init__(self):
+        Spell.__init__(self, 'Confundo', 'Confundus Charm')
+
+    def get_description(self):
+        return 'Causes the victim to become confused and befuddled.'
+
+def study_spell(spell):
+    print (spell)
+
+#spell = Accio()
+#spell.execute()
+#study_spell(spell)
+#study_spell(Accio())
+study_spell(Confundo())
+print (Accio())
